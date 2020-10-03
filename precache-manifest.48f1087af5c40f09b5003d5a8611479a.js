de46bf012acc3d7ec3108ad390279754afa04a3f
Boris <boris2000n@gmail.com>
2020-10-03 21:52:13 +0300

    frontend

---
+++ precache-manifest.48f1087af5c40f09b5003d5a8611479a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9185b888675bbd3953c055df6a94648",
+    "url": "/index.html"
+  },
+  {
+    "revision": "985a6ddf9435b89800b4",
+    "url": "/static/css/main.3a9583ca.chunk.css"
+  },
+  {
+    "revision": "5cb004ec0db0d8c8c8ca",
+    "url": "/static/js/2.dcaf30a1.chunk.js"
+  },
+  {
+    "revision": "8eb8fa83967b388e40d9a7f20e622076",
+    "url": "/static/js/2.dcaf30a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "985a6ddf9435b89800b4",
+    "url": "/static/js/main.4d9c4067.chunk.js"
+  },
+  {
+    "revision": "5a31a3951fdf35ae30b0",
+    "url": "/static/js/runtime-main.d0570db3.js"
+  },
+  {
+    "revision": "cba0b89d2bf2d96a1ed26edb5849f804",
+    "url": "/static/media/logo-og.cba0b89d.png"
+  }
+]);
